4427c6d8813c359355cd0c80e85ec5ba4b0d6b6c
unknown <archangel.raffael@gmail.com>
2023-01-09 01:36:06 -0700

    second function for the blue button

--- include/fuel_injector_signal.h
+++ include/fuel_injector_signal.h
@@ -7,8 +7,11 @@
 
 bool state_is_fuel_injector_open = false;
 bool state_is_red_button_down = false;
+bool state_is_blue_button_down = false;
 unsigned long red_button_press_time = 0;
 unsigned long red_button_release_time = 0;
+unsigned long blue_button_press_time = 0;
+unsigned long blue_button_release_time = 0;
 
 void init_fuel_injector() {
     pinMode(FUEL_INJECTOR_PIN, OUTPUT);
@@ -20,6 +23,11 @@ void init_fuel_injector() {
 void reset_red_button() {
     red_button_press_time = 0;
     red_button_release_time = 0;
+}
+
+void reset_blue_button() {
+    blue_button_press_time = 0;
+    blue_button_release_time = 0;
 }
 
 bool is_blue_button_down() {
@@ -40,6 +48,12 @@ void close_fuel_injector() {
     state_is_fuel_injector_open = false;
 }
 
+void open_fuel_injector(uint32_t time_ms) {
+    open_fuel_injector();
+    delay(time_ms);
+    close_fuel_injector();
+}
+
 void trigger_fuel_injector_single_open_signal() {
     delay(FUEL_INJECTOR_OPEN_DELAY_MS);
     open_fuel_injector();
@@ -56,35 +70,60 @@ void trigger_fuel_injector_multiple_open_signal(const uint8_t times = 1) {
     }
 }
 
+bool event_blue_button_pressed() {
+    return blue_button_release_time > blue_button_press_time;
+}
+
+bool event_red_button_pressed() {
+    return red_button_release_time > red_button_press_time;
+}
+
 void fuel_injector_loop() {
-    if (is_blue_button_down()) {
-        // blue button pressed
-        trigger_fuel_injector_single_open_signal();
-        while (is_blue_button_down()) {
-            delay(1);
-        }
+    if (is_blue_button_down() && !state_is_blue_button_down) {
+        blue_button_press_time = millis();
+        state_is_blue_button_down = true;
+    }
+
+    if (!is_blue_button_down() && state_is_blue_button_down) {
+        blue_button_release_time = millis();
+        state_is_blue_button_down = false;
     }
 
     if (is_red_button_down() && !state_is_red_button_down) {
         state_is_red_button_down = true;
         red_button_press_time = millis();
     }
-    
+
     if (!is_red_button_down() && state_is_red_button_down) {
         state_is_red_button_down = false;
         red_button_release_time = millis();
     }
 
-    if (red_button_release_time > red_button_press_time) {
+    if (event_red_button_pressed()) {
         // red button pressed
-        uint16_t time_ms = red_button_release_time - red_button_press_time;
-        if (time_ms <= 2000) {
+        uint16_t red_press_time_ms = red_button_release_time - red_button_press_time;
+        if (red_press_time_ms <= 2000) {
             trigger_fuel_injector_multiple_open_signal(5);
             reset_red_button();
         }
-        if (time_ms > 2000) {
+        if (red_press_time_ms > 2000) {
             trigger_fuel_injector_multiple_open_signal(100);
             reset_red_button();
+        }
+    }
+
+    if (event_blue_button_pressed()) {
+        // blue button pressed
+        uint16_t blue_press_time_ms = red_button_release_time - red_button_press_time;
+        if (blue_press_time_ms <= 2000) {
+            trigger_fuel_injector_single_open_signal();
+            reset_blue_button();
+            delay(100);
+        }
+        if (blue_press_time_ms > 2000) {
+            open_fuel_injector(3000);
+            reset_blue_button();
+            delay(1000);
         }
     }
 }
